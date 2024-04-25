f59bc16f30bd24555b0efde556e14e2aab69e25c
ty.luo <ty.luo@shunwang.com>
2024-04-25 16:35:39 +0800

    add csv plugin.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-    
+use clap::Parser;
+use rcli::{Opts, process_csv, Subcommand};
+
+fn main() -> anyhow::Result<()> {
+    let opt = Opts::parse();
+    match opt.cmd {
+        Subcommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
